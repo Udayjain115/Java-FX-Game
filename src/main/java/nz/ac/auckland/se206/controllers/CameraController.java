f958c91183b7f7dea82428d07556ddb407dbbfeb
Oshan Premkumar <opre469@aucklanduni.ac.nz>
2024-09-16 01:12:53 +1200

    added code to exit camera clue and resized the scene

--- src/main/java/nz/ac/auckland/se206/controllers/CameraController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CameraController.java
@@ -1,5 +1,30 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.App;
+
 public class CameraController {
+  @FXML Rectangle forward;
+  @FXML Rectangle back;
+  @FXML Rectangle exit;
+  @FXML ImageView twocamera;
+  @FXML ImageView onecamera;
+  @FXML ImageView twelvecamera;
+  @FXML ImageView elevencamera;
+  @FXML ImageView tencamera;
+
   
+  public void initialize(){
+    forward.setDisable(true);
+  }
+
+  public void onExit() throws IOException{
+    App.setRoot("crimeScene");
+  }
+
+
 }
