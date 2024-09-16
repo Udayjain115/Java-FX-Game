d2d7d6a58758bcb03afa3f706323e16fbeafd132
Josh <1joshts@gmail.com>
2024-09-17 01:07:04 +1200

    Adding an image cursor, so that the user knows to dust off the book.

--- src/main/java/nz/ac/auckland/se206/controllers/RulebookController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RulebookController.java
@@ -3,6 +3,9 @@ package nz.ac.auckland.se206.controllers;
 import java.io.IOException;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
@@ -11,6 +14,39 @@ public class RulebookController {
 
   @FXML private ImageView dustImage;
   @FXML private ImageView dustImage2;
+
+  private ImageCursor dustBrushCursor;
+
+  @FXML
+  public void initialize() {
+    // Load the original cursor image
+    Image originalCursorImage = new Image(getClass().getResourceAsStream("/images/brush.png"));
+
+    // Create an ImageCursor with the scaled image
+    dustBrushCursor =
+        new ImageCursor(
+            originalCursorImage, originalCursorImage.getWidth(), originalCursorImage.getHeight());
+
+    // Set event handlers for dustImage
+    dustImage.setOnMouseEntered(
+        event -> {
+          dustImage.setCursor(dustBrushCursor);
+        });
+    dustImage.setOnMouseExited(
+        event -> {
+          dustImage.setCursor(Cursor.DEFAULT);
+        });
+
+    // Set event handlers for dustImage2
+    dustImage2.setOnMouseEntered(
+        event -> {
+          dustImage2.setCursor(dustBrushCursor);
+        });
+    dustImage2.setOnMouseExited(
+        event -> {
+          dustImage2.setCursor(Cursor.DEFAULT);
+        });
+  }
 
   public void dustClickedOn() {
     FadeTransition ft = new FadeTransition(Duration.millis(1000), dustImage);
