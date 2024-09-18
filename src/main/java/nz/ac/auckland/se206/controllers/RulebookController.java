7e966542985a27d8b601a7e5599b5b6e7f7fda5f
Oshan Premkumar <opre469@aucklanduni.ac.nz>
2024-09-18 15:48:56 +1200

    moved position of the timer label and changed text inside

--- src/main/java/nz/ac/auckland/se206/controllers/RulebookController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RulebookController.java
@@ -65,7 +65,7 @@ public class RulebookController {
         int time = timer.getTimeLeft().get();
         int mins = time/60;
         int secs = time % 60;
-        return String.format("%1d:%02d", mins, secs);
+        return String.format("%s: %1d:%02d","Time Left", mins, secs);
       },timer.getTimeLeft());
 
       timerLbl.textProperty().bind(timeLayout);
--- src/main/java/nz/ac/auckland/se206/controllers/SuspectRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SuspectRoomController.java
@@ -59,7 +59,7 @@ public class SuspectRoomController {
         int time = timer.getTimeLeft().get();
         int mins = time/60;
         int secs = time % 60;
-        return String.format("%1d:%02d", mins, secs);
+        return String.format("%s: %1d:%02d","Time Left", mins, secs);
       },timer.getTimeLeft());
 
       timerLbl.textProperty().bind(timeLayout);
