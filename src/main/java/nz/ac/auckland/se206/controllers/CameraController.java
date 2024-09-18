0cc835d2cfa0a7e5af1834ebf7a80af62f3497e9
Oshan Premkumar <opre469@aucklanduni.ac.nz>
2024-09-18 23:49:01 +1200

    changed the camera visuals to fit with new background

--- src/main/java/nz/ac/auckland/se206/controllers/CameraController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CameraController.java
@@ -1,10 +1,8 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
-
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringBinding;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -13,7 +11,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.Timer;
 
@@ -30,77 +27,71 @@ public class CameraController {
 
   private int count = 5;
 
-  
-  public void initialize(){
+  public void initialize() {
     forward.setDisable(true);
 
     Timer timer = Timer.getTimer();
-      StringBinding timeLayout = Bindings.createStringBinding(() -> {
-        int time = timer.getTimeLeft().get();
-        int mins = time/60;
-        int secs = time % 60;
-        return String.format("%s: %1d:%02d","Time Left", mins, secs);
-      },timer.getTimeLeft());
+    StringBinding timeLayout =
+        Bindings.createStringBinding(
+            () -> {
+              int time = timer.getTimeLeft().get();
+              int mins = time / 60;
+              int secs = time % 60;
+              return String.format("%s: %1d:%02d", "Time Left", mins, secs);
+            },
+            timer.getTimeLeft());
 
-      timerLbl.textProperty().bind(timeLayout);
-      timer.start();
+    timerLbl.textProperty().bind(timeLayout);
+    timer.start();
   }
 
-  public void onExit(MouseEvent event) throws IOException{
-    
+  public void onExit(MouseEvent event) throws IOException {
+
     Parent crimeSceneRoot = SceneManager.getUiRoot(SceneManager.AppUi.CRIME_SCENE);
     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-  
-
     stage.getScene().setRoot(crimeSceneRoot);
-    
-  
   }
 
-  public void onGoBack(){
-    if(count == 5){
+  public void onGoBack() {
+    if (count == 5) {
       twocam.setVisible(false);
       forward.setDisable(false);
       count--;
       return;
-    }else if(count == 4){
+    } else if (count == 4) {
       onecam.setVisible(false);
       count--;
       return;
-    }else if(count == 3){
+    } else if (count == 3) {
       twelvecam.setVisible(false);
       count--;
       return;
-    }else if(count == 2){
+    } else if (count == 2) {
       elevencam.setVisible(false);
       count--;
       back.setDisable(true);
     }
-    
-    
   }
 
-  public void onGoForward(){
-    if(count == 4){
+  public void onGoForward() {
+    if (count == 4) {
       twocam.setVisible(true);
       forward.setDisable(true);
       count++;
       return;
-    }else if(count == 3){
+    } else if (count == 3) {
       onecam.setVisible(true);
       count++;
       return;
-    }else if(count == 2){
+    } else if (count == 2) {
       twelvecam.setVisible(true);
       count++;
       return;
-    }else if(count == 1){
+    } else if (count == 1) {
       elevencam.setVisible(true);
       count++;
       back.setDisable(false);
     }
   }
-
-
 }
